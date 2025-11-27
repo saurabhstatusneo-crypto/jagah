4810dd92944dd6e707c490e17734a59711d23b38
saurabh statusneo <saurabhstatusneo@gmail.com>
2025-11-27 12:01:00 +0530

    jagah

---
+++ src/main/java/com/example/jagah/JagahApplication.java
@@ -0,0 +1,13 @@
+package com.example.jagah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagahApplication.class, args);
+	}
+
+}
